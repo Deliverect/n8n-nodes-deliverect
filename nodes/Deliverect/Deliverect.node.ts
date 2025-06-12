cc7f2e3af142f16a51a55a2bfae0918f4014c188
Stefaan Ghysels <stefaan.ghysels@deliverect.com>
2025-06-12 15:17:05 +0200

    First version

---
+++ nodes/Deliverect/Deliverect.node.ts
@@ -0,0 +1,198 @@
+import type {
+	INodeType,
+	INodeTypeDescription,
+} from 'n8n-workflow';
+import { NodeConnectionType } from 'n8n-workflow';
+
+export class Deliverect implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Deliverect',
+		name: 'deliverect',
+		icon: 'file:deliverect.svg',
+		group: ['transform'],
+		version: 1,
+		description: 'Consume Deliverect API',
+		subtitle: '={{ $parameter["operation"] + ": " + $parameter["resource"] }}',
+		defaults: {
+			name: 'Deliverect',
+		},
+		inputs: [NodeConnectionType.Main],
+		outputs: [NodeConnectionType.Main],
+		credentials: [
+			{
+				name: 'deliverectApi',
+				required: true,
+			},
+		],
+		requestDefaults: {
+			baseURL: '=https://{{$credentials.domain}}',
+			headers: {
+				Accept: 'application/json',
+				'Content-Type': 'application/json',
+			},
+		},
+		usableAsTool: true,
+		properties: [
+			{
+				displayName: 'Resource',
+				name: 'resource',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Store API',
+						value: 'storeAPI',
+					},
+					{
+						name: 'Channel API',
+						value: 'channelAPI',
+					},
+					{
+						name: 'KDS API',
+						value: 'kdsAPI',
+					},
+					{
+						name: 'POS API',
+						value: 'posAPI',
+					}
+				],
+				default: 'storeAPI',
+			},
+		// Operations will go here
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: [
+							'storeAPI',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Get Stores',
+						value: 'getStores',
+						action: 'Get stores',
+						description: 'Get stores for an account',
+						routing: {
+							request: {
+								method: 'GET',
+								url: '/locations',
+							},
+						},
+					},
+					{
+						name: 'Get Out-Of-Stock Products',
+						value: 'getOutOfStock',
+						action: 'Get out-of-stock products',
+						description: 'Get out-of-stock products for a location',
+						routing: {
+							request: {
+								method: 'GET',
+								url: '=/channelDisabledProducts?where={"account":"{{$account}}","location":"{{$location}}"}'
+							},
+						},
+					},
+					{
+						name: 'Set Out-of-stock Products',
+						value: 'setOutOfStock',
+						action: 'Set out-of-stock products',
+						description: 'Set out-of-stock products for a location',
+						routing: {
+							request: {
+								method: 'POST',
+								url: '/products/snoozeByPlus'
+							},
+						},
+					},
+					{
+						name: 'Set Store Status',
+						value: 'setStoreStatus',
+						action: 'Set store status',
+						description: 'Set store status for a location',
+						routing: {
+							request: {
+								method: 'POST',
+								url: '=/updateStoreStatus/{{$location}}'
+							},
+						},
+					},
+					{
+						name: 'Get Store Opening Hours',
+						value: 'getStoreOpeningHours',
+						action: 'Get store opening hours',
+						routing: {
+							request: {
+								method: 'GET',
+								url: '=/account/{{$account}}/openingHours'
+							},
+						},
+					},
+					
+				],
+				default: 'getStores',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: [
+							'channelAPI',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Create Order',
+						value: 'createOrder',
+						action: 'Create order',
+						description: 'Create an order to a store',
+						routing: {
+							request: {
+								method: 'POST',
+								url: '=/deliverect/order/{{$store}}'
+							},
+						},
+					},
+				],
+				default: 'createOrder',
+			},
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				displayOptions: {
+					show: {
+						resource: [
+							'posAPI',
+						],
+					},
+				},
+				options: [
+					{
+						name: 'Sync POS Products',
+						value: 'productSync',
+						action: 'Sync POS products',
+						description: 'Sync products for a location',
+						routing: {
+							request: {
+								method: 'POST',
+								url: '/productAndCategories'
+							},
+						},
+					},
+				],
+				default: 'productSync',
+			},
+			// Optional/additional fields will go here
+		],
+	};
+	
+}
